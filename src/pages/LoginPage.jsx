f4b24ab4253f077c0488161cf02e5b03cfa4b12b
victor <victorcalehr18@gmail.com>
2026-03-08 23:54:01 -0500

    added email box

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,13 +6,34 @@ export default function LoginPage({ onLogin }) {
     return (
         <div className="login-wrap">
           <div className="login-card">
-            {/* Logo */}
+            {/* logo */}
             <div className="login-logo">
               <div className="login-logo-icon">
                 <GlobeIcon />
               </div>
               <h1>Wanderwise</h1>
             </div>
+        
+        {/* email box */}
+                <div className="field-grp">
+                  <label>Email</label>
+                  <div className="field-wrap">
+                    <div className="field-icon"><MailIcon /></div>
+                    <input
+                      type="email"
+                      placeholder="your@email.com"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      onKeyDown={(e) => e.key === "Enter" && handle()}
+                      autoComplete="email"
+                    />
+                  </div>
+                </div>
+
+
+
+
+
         </div>
         </div>
     )
