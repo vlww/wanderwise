338a3c33d7419bd8cdce097a8c37c7e873b9df1f
victor <victorcalehr18@gmail.com>
2026-03-10 01:37:51 -0500

    added sample data for 3 destinations

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,2 +1,31 @@
 export const DEMO_EMAIL    = "traveler@wanderwise.app";
 export const DEMO_PASSWORD = "wanderwise123";
+
+export const INITIAL_WISHLIST = [
+  {
+    id: 1,
+    name: "Atlanta",
+    country: "United States",
+    cost: 500,
+    fav: true,
+    img: "https://media.istockphoto.com/id/636101348/photo/atlanta-georgia-skyline.jpg?s=612x612&w=0&k=20&c=THysyIF3mwZObg_SoNmcOzJ6GAIFba5uJncB2yWnPXc=",
+  },
+  {
+    id: 2,
+    name: "Cancún",
+    country: "Mexico",
+    cost: 1100,
+    fav: true,
+    img: "https://t4.ftcdn.net/jpg/02/14/74/11/360_F_214741146_jApjeGkU3GSPSpLhzyMsupP7frpAh1XH.jpg",
+  },
+  {
+    id: 3,
+    name: "Paris",
+    country: "France",
+    cost: 1800,
+    fav: true,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/640px-Paris_Night.jpg",
+  },
+];
+
+export const INITIAL_SAVINGS = 1200;
